9d68a9ef4c31c41854fcff824c049e2c23eb6c0a
Vladimir Kyuranov <the_crow@abv.bg>
2018-09-28 11:53:16 +0300

    +

--- Exercises/06-Lab-ArraysAndMatrices/07-SmallestTwoNumbers.js
+++ Exercises/06-Lab-ArraysAndMatrices/07-SmallestTwoNumbers.js
@@ -1,8 +1,8 @@
 function solve(input) {
-    let result = input.sort((a, b) => a - b);
-    result = result.slice(0, 2);
-
-    return result.join(' ');
+    return input
+        .sort((a, b) => a - b)
+        .slice(0, 2)
+        .join(' ');
 }
 
 console.log(solve([30, 15, 50, 5]));
